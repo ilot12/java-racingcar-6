8940a4811ed6ec1ca67a0968ded3ee62aa649848
ilot12 <rlaehdwn60@gmail.com>
2023-12-15 12:11:05 +0900

    add: inputView, outputView클래스 작성
    각 입력과 출력 담당하는 클래스 작성

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racer.Racer;
+
+public class OutputView {
+    
+    public static void resultInitialMessage() {
+        System.out.println("\n실행결과");
+    }
+    
+    public static void printRacerAdvance(Racer racer) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(racer.getName());
+        sb.append(" : ");
+        int i = 0;
+        for (i = 0;i < racer.getAdvance(); i++) {
+            sb.append("-");
+        }
+        System.out.println(sb.toString());
+    }
+    
+    public static void printWinners(List<String> winnerNames) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("최종 우승자 : ");
+        sb.append(String.join(", ", winnerNames));
+        System.out.println(sb.toString());
+    }
+}
